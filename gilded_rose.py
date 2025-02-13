9a92e1ea66961b2f9813999119f3172e5cf9f0b3
Leona202161 <bowanglin@gmail.com>
2025-02-12 20:59:14 -0800

    updated Lab 5

--- gilded_rose.py
+++ gilded_rose.py
@@ -1,4 +1,8 @@
 # -*- coding: utf-8 -*-
+
+from abc import ABC, abstractmethod
+
+
 class Item:
     """ DO NOT CHANGE THIS CLASS!!!"""
     def __init__(self, name, sell_in, quality):
@@ -10,8 +14,59 @@ class Item:
         return "%s, %s, %s" % (self.name, self.sell_in, self.quality)
 
 
-class GildedRose(object):
+# Quality Update Strategy Interface
+class QualityUpdateStrategy(ABC):
+    @abstractmethod
+    def update_quality(self, item):
+        pass
 
+
+# Concrete Strategy for Sulfuras (no change in quality)
+class SulfurasStrategy(QualityUpdateStrategy):
+    def update_quality(self, item):
+        pass
+
+
+# Concrete Strategy for Aged Brie (increases in quality)
+class AgedBrieStrategy(QualityUpdateStrategy):
+    def update_quality(self, item):
+        if item.quality < 50:
+            item.quality += 1
+
+
+# Concrete Strategy for Backstage Passes (quality increases as sell_in decreases)
+class BackstagePassesStrategy(QualityUpdateStrategy):
+    def update_quality(self, item):
+        if item.sell_in <= 0:
+            item.quality = 0
+        elif item.sell_in <= 5:
+            if item.quality < 50:
+                item.quality += 3
+        elif item.sell_in <= 10:
+            if item.quality < 50:
+                item.quality += 2
+        else:
+            if item.quality > 0:
+                item.quality -= 1
+
+
+# Concrete Strategy for Conjured Items (degrade twice as fast)
+class ConjuredItemStrategy(QualityUpdateStrategy):
+    def update_quality(self, item):
+        if item.quality > 1:  # Ensure the item does not go below 0
+            item.quality -= 2
+        else:
+            item.quality = 0
+
+
+# Concrete Strategy for Normal Items (degrade normally)
+class NormalItemStrategy(QualityUpdateStrategy):
+    def update_quality(self, item):
+        if item.quality > 0:
+            item.quality -= 1
+
+
+class GildedRose(object):
     def __init__(self, items: list[Item]):
         # DO NOT CHANGE THIS ATTRIBUTE!!!
         self.items = items
@@ -22,31 +77,19 @@ class GildedRose(object):
             if item.name == "Sulfuras, Hand of Ragnaros":
                 continue  # Skip Sulfuras processing
 
-            # Handle Aged Brie
-            if item.name == "Aged Brie":
-                if item.quality < 50:
-                    item.quality += 1
-
-            # Handle Backstage passes to a TAFKAL80ETC concert
+            # Call strategy based on item type
+            if "Conjured" in item.name:
+                conjured_item_strategy = ConjuredItemStrategy()
+                conjured_item_strategy.update_quality(item)
+            elif item.name == "Aged Brie":
+                aged_brie_strategy = AgedBrieStrategy()
+                aged_brie_strategy.update_quality(item)
             elif item.name == "Backstage passes to a TAFKAL80ETC concert":
-                if item.sell_in <= 0:
-                    item.quality = 0  # Quality drops to 0 after the concert
-                elif item.sell_in <= 5:
-                    if item.quality < 50:
-                        item.quality += 3  # Quality increases by 3 if there are 5 days or less
-                elif item.sell_in <= 10:
-                    if item.quality < 50:
-                        item.quality += 2  # Quality increases by 2 if there are 10 days or less
-
-            # Handle Conjured items (degrade twice as fast)
-            elif "Conjured" in item.name:
-                if item.quality > 0:
-                    item.quality -= 2
-
-            # Handle Normal items (degrade normally)
+                backstage_passes_strategy = BackstagePassesStrategy()
+                backstage_passes_strategy.update_quality(item)
             else:
-                if item.quality > 0:
-                    item.quality -= 1
+                normal_item_strategy = NormalItemStrategy()
+                normal_item_strategy.update_quality(item)
 
             # Decrease sell_in for all items except Sulfuras
             if item.name != "Sulfuras, Hand of Ragnaros":
